8199c37a3db724221e8792e9315d787792202585
Vibhaj Rajan <vibhaj8@gmail.com>
2014-04-01 01:25:56 +0530

    finished porting

--- ui/js/app.js
+++ ui/js/app.js
@@ -3,21 +3,36 @@ var APP = angular.module('APP', ['ngRoute', 'ngSanitize'])
 	.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider){
 
 		$routeProvider
-			.when('/', {templateUrl: 'ui/tpl/home.html', controller: "init"});
+			.when('/', {templateUrl: 'ui/tpl/view.html', controller: "init"})
+
+			.when('/:page', {templateUrl: 'ui/tpl/view.html',
+				controller: ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $routeParams) {
+					$rootScope.page = $routeParams.page;
+					$scope.pageURL = 'ui/tpl/'+ $rootScope.page +'.html';
+				}]
+			});
 		
 		$httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
 		$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 	}])
 
-	.controller('init', ['$scope', '$timeout', '$location',
+	.controller('init', ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $routeParams) {
+		$scope.minHeight=$(window).height()-3;
+		$scope.headerURL = 'ui/tpl/header.html';
+		$scope.footerURL = 'ui/tpl/footer.html';
 
-		function($scope,$resource,API,$timeout,$location) {
-			$scope.minHeight=$(window).height()-3;
-			$scope.headerURL = 'ui/tpl/header.html';
-		}
+		$scope.menu = [ 'home', 'labs', 'hire', 'about' ];
+		$scope.menu_urls = {
+			home: '',
+			labs: 'labs',
+			hire: 'hire',
+			about: 'about',
+		};
 
-	]);
+		$rootScope.page = 'home';
+		$scope.pageURL = 'ui/tpl/home.html';
+	}]);
 
 
 $( document ).ready( function(){
